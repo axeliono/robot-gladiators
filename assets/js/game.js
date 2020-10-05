31bac8ed316a08472ebe9ff55a37f36b0c3c2765
Axeliono <69375446+axeliono@users.noreply.github.com>
2020-10-05 18:39:40 -0500

    began adding to .js, studied variable types, etc.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert! Javascript is running!")
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+
+console.log(10+10);
+console.log("Our robot's name is " + playerName);
+
+function fight(){
+    window.alert("the fight has begun!")
+}
+
+fight();
