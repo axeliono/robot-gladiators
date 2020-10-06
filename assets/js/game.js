148e942d537f1d2798eed9ecf45321abb8ace677
Axeliono <69375446+axeliono@users.noreply.github.com>
2020-10-05 19:19:44 -0500

    began and finished basic fight function and variables

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,11 +1,40 @@
 var playerName = window.prompt("What is your robot's name?");
-console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(10+10);
-console.log("Our robot's name is " + playerName);
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-function fight(){
-    window.alert("the fight has begun!")
-}
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+  window.alert("Welcome to Robot Gladiators!");
+  enemyHealth = enemyHealth - playerAttack;
+  playerHealth = playerHealth - enemyAttack;
+  console.log(
+  enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+  );
+  //check player's health
+  if(playerHealth <=0) {
+      window.alert(playerName + " has died!");
+  }
+  else {
+      window.alert(playerName + " still has " + playerHealth + " health left.")
+  }
+  console.log(
+      playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+  );
+
+  //check enemy's health
+  if(enemyHealth <= 0) {
+      window.alert(enemyName + " has died!");
+  }
+  else {
+      window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+};
 
 fight();
+
